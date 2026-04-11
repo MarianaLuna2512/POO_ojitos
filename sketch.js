9c1175c17090d04852515ad21681def17646c233
Mariana Luna <amcorreal@libertadores.edu.co>
2026-04-11 15:25:37 -0500

    detección ojos, ok

--- sketch.js
+++ sketch.js
@@ -11,7 +11,6 @@ class FiguraOjos {
  }
 
 
-
  show() {
 
   fill(255);
@@ -37,32 +36,63 @@ class FiguraOjos {
 }
 
 
-
 let figura;
-
+let video;
+let faceMesh;
+let eyeX = 200;
+let eyeY = 200;
 
 
 function setup() {
+  createCanvas(400, 400);
+  frameRate(24);
 
- createCanvas(400, 400);
+  video = createCapture(VIDEO);
+  video.size(400, 400);
+  video.hide();
 
- frameRate(24);
+  figura = new FiguraOjos(200, 200, 60);
 
- figura = new FiguraOjos(200, 200, 60);
+  faceMesh = new FaceMesh({
+    locateFile: (file) => `https://cdn.jsdelivr.net/npm/@mediapipe/face_mesh/${file}`
+  });
 
+  faceMesh.setOptions({
+    maxNumFaces: 1,
+    refineLandmarks: true,
+    minDetectionConfidence: 0.5,
+    minTrackingConfidence: 0.5
+  });
+
+  faceMesh.onResults(onResults);
+
+  const camera = new Camera(video.elt, {
+    onFrame: async () => {
+      await faceMesh.send({ image: video.elt });
+    },
+    width: 400,
+    height: 400
+  });
+  camera.start();
 }
 
+
+function onResults(results) {
+  if (results.multiFaceLandmarks && results.multiFaceLandmarks.length > 0) {
+    const landmarks = results.multiFaceLandmarks[0];
+    const leftEye = landmarks[33];
+    const rightEye = landmarks[263];
+
+    eyeX = map((leftEye.x + rightEye.x) / 2, 0, 1, width, 0);
+    eyeY = map((leftEye.y + rightEye.y) / 2, 0, 1, 0, height);
+  }
+}
 
 
 function draw() {
+  background(45, 150, 200);
 
- background(45, 150, 200);
-
- figura.x = mouseX;
-
- figura.y = mouseY;
-
- figura.show();
-
+  figura.x = eyeX;
+  figura.y = eyeY;
+  figura.show();
 }
-
